8d4c0c5459486ce111d2751b3304e285df9b2924
badr ghanbi <bghanbi50@gmail.com>
2023-03-08 12:24:33 +0100

    add the search bar

--- src/Component/header/SearchBar.jsx
+++ src/Component/header/SearchBar.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
-
+import { SearchIcon } from './SearchIcon'
 export const SearchBar = () => {
   return (
-    <div className='text-red-700'>SearchBar</div>
+    <form  className='border-2 bg-white  flex items-center justify-between py-2 px-4 rounded-full'>
+   <input className='outline-none text-black  bg-transparent'  type="text" placeholder='Search ' />
+    <SearchIcon />
+    </form>
   )
 }
