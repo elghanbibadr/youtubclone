56dd645db6bc84c9d58ef79495a79637126d0837
badr ghanbi <bghanbi50@gmail.com>
2023-03-10 11:45:25 +0100

    add the mobile header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './Component/header/Header'
 import { SearchBar } from './Component/header/SearchBar'
 function App() {
  
  return  <>
-      <h1>hello world</h1>
+     <Header/>
      </>
  
 }
---
+++ src/Component/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SearchBar } from './SearchBar'
+import youtubLogo from "../../assets/youtbLogo.png"
+const Header = () => {
+  return (
+    <header className='flex items-center justify-between'>
+     <img className='h-10' src={youtubLogo} alt="youtub logo" />
+     <SearchBar/>
+    </header>
+  )
+}
+
+export default Header
