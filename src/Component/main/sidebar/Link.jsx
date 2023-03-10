2494a5023603bcc75fcaf8a58974b0b731b4264c
badr ghanbi <bghanbi50@gmail.com>
2023-03-10 17:39:33 +0100

    more shit fix

--- src/Component/main/sidebar/Link.jsx
+++ src/Component/main/sidebar/Link.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 export const Link = (props) => {
   return (
-    <li className='flex items-center   mr-14'>
+    <li className='flex items-center pb-2   mr-20'>
     <img className='h-6 mr-2' src={props.src} />
     <p className='text-white w-fit text-sm '>{props.name}</p>
     </li>
--- src/Component/main/sidebar/SideBar.jsx
+++ src/Component/main/sidebar/SideBar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
  import { linkList } from '../../../constant'
 import { Link } from './Link'
+
 export const SideBar = () => {
   return (
-    <div>
-      <ul className='flex items-center overflow-y-auto'>
+    <div >
+      <ul className='flex mt-8 items-center overflow-y-auto'>
          {linkList.map(({name,src},index)=>{
           return <Link key={index} name={name} src={src}  />
         })} 
